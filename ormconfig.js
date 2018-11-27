cc16c22e300fe4ce8a10e5db5680341421b89892
moKa <mokacraft@gmail.com>
2018-11-27 21:03:16 +0100

    add typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,10 @@
 module.exports = {
     "type": "mongodb",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE,
     "synchronize": true,
     "logging": false,
     "useNewUrlParser": true,
