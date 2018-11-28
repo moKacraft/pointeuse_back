5e9d964ba50aeeb60b541027f5622eeedc2ba02a
moKa <mokacraft@gmail.com>
2018-11-28 21:45:17 +0100

    test heroku mongolab connect with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,18 @@ import { Connection } from 'typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_HOST),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    logging: true,
+    synchronize: true,
+  useNewUrlParser: true,
+  entities: ['dist/**/**.entity.js']}),
+  UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
